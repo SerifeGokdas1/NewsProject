6c860ad5ebd55504cf0b6834681b95ca419927d4
Serife Gokdas <serifegokdas@hotmail.com>
2023-12-16 01:13:25 +0300

    created services folder

---
+++ NewsProject/Services/DetailService.cs
@@ -0,0 +1,26 @@
+﻿using NewsProject.Model.Models;
+using Newtonsoft.Json;
+
+namespace NewsProject.Services
+{
+    public class DetailService
+    {
+        private readonly HttpClient _httpClient;
+        public DetailService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Details> GetDetail(string itemId)
+        {
+            // API'den JSON verilerini çekme işlemi yaptım
+            string detailUrl = $"https://www.turkmedya.com.tr/detay.json?itemId={itemId}";
+            string jsonString = await _httpClient.GetStringAsync(detailUrl);
+
+            // Çektiğim JSON verisini Details nesnesine çevirdim 
+            Details haberDetay = JsonConvert.DeserializeObject<Details>(jsonString);
+
+            return haberDetay;
+        }
+    }
+}
---
+++ NewsProject/Services/NewsService.cs
@@ -0,0 +1,30 @@
+﻿using NewsProject.Model.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace NewsProject.Services
+{
+    public class NewsService
+    {
+        private readonly HttpClient _httpClient;
+
+        public NewsService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<News>> GetHaberler()
+        {
+            // API'den JSON verisini çektim
+            string jsonUrl = "https://www.turkmedya.com.tr/anasayfa.json";
+            string jsonString = await _httpClient.GetStringAsync(jsonUrl);
+
+            // JSON verisini News listesine çevirdim
+            List<News> haberler = JsonConvert.DeserializeObject<List<News>>(jsonString);
+
+            return haberler;
+        }
+
+        
+    }
+}
